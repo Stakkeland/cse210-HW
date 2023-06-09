0413302458508874f6e4ca50a16542352ac06ad8
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-09 12:18:38 -0700

    finished prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        double num = 1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<double> numbers = new List<double>();
+
+        while (num != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            string guessInText = Console.ReadLine();
+            num = int.Parse(guessInText);
+
+            numbers.Add(num);
+
+        }
+        
+        //pop 0 off
+        numbers.RemoveAt(numbers.Count - 1);
+
+        //sum
+        double sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        //average
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        //largest
+        double max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
