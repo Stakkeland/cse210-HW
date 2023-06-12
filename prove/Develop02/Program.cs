17d35d8f1eda12ceb3c4284defb5424efd477158
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-11 20:43:53 -0700

    Menu system done

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int answer = 0;
+        while (answer != 5)
+        {
+            string multiline = @"Please select one of the following:
+            1.Write new entry
+            2.Display journal
+            3.Save
+            4.Load journal
+            5.Quit";
+            Console.WriteLine(multiline);
+            string answerInText = Console.ReadLine();
+            answer = int.Parse(answerInText);
+
+            if (answer == 1)
+            {
+                //Entry class
+                return;
+            }
+            else if (answer == 2)
+            {
+                //journal class
+                return;
+            }
+            else if (answer == 3)
+            {
+                //journal class
+                return;
+            }
+            else if (answer == 4)
+            {
+                //journal class
+                return;
+            }
+            else if (answer == 5)
+            {
+                return;
+            }
+            else 
+            {
+                Console.WriteLine("Sorry that is not an option please try again.");
+            }
+        }
     }
 }
