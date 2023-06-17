fe6b500432284eac41b6a6cb12b2921c0448769b
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-16 19:46:36 -0700

    completed outline for Journal Class

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,26 @@
+//Class Journal
+
+public class Journal
+{
+    public List<> _entries = new List<>();
+
+    public void AddEntry()
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile()
+    {
+
+    }
+
+    public void LoadFromFile()
+    {
+
+    }
+}
