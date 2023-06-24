685e97428a106b17a6f67fcd1c8f2f401dc32bb9
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-24 11:46:20 -0700

    small revision to reference and scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         string userInput = "";
-        Reference _reference = new Reference("Proverbs", 3, 5, 2, 6);
-        Scripture scripture = new Scripture(_reference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        Reference _reference2 = new Reference("Proverbs", 3, 5, 2, 6);
+        Scripture scripture2 = new Scripture(_reference2, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
 
-        Reference _reference2 = new Reference("1st Nephi", 1, 12);
-        Scripture scripture2 = new Scripture(_reference2, "And it came to pass that as he read, he was filled with the Spirit of the Lord.");
+        Reference _reference = new Reference("1st Nephi", 1, 12);
+        Scripture scripture = new Scripture(_reference, "And it came to pass that as he read, he was filled with the Spirit of the Lord.");
         while (userInput != "quit")
         {
-            Console.WriteLine(scripture2.GetDisplayText());
+            Console.WriteLine(scripture.GetDisplayText());
             Console.WriteLine();
             Console.WriteLine("Press enter to continue or type 'quit' to finish");
             userInput = Console.ReadLine();
