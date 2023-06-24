79d87d9271cf9d4711dcb20f3d8d9d871cb9b4a1
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-23 19:59:57 -0700

    finish word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,34 +1,34 @@
 
-class Words
+class Word
 {
     //variables
     private string _text;
     private bool _isHidden;
 
     //constructor
-    public Words (string _text)
+    public Word (string text)
     {
-
+        _text = text;
     }
 
     //methods
     private void Hide()
     {
-
+        _isHidden = true;
     }
 
     public void Show()
     {
-
+        _isHidden = false;
     }
 
     public bool IsHidden()
     {
-        return true;
+        return _isHidden;
     }
 
     public string GetDisplayText()
     {
-        return "";
+        return _text;
     }
 }
