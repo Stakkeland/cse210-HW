7def9a51eb5db65734df775cb7580001dbff3e3d
Stakkeland <sawyerstakkeland@gmail.com>
2023-06-23 20:31:35 -0700

    fixed scripture reference

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,8 +5,8 @@ class Program
     static void Main(string[] args)
     {
         string userInput = "";
-        Reference _reference = new Reference("Proverbs", 3, 5, 6, 10);
-        Scripture scripture = new Scripture(_reference, "Trust in the lord with all thine understanding; om a;; thy ways acknowledge him, and he shall direct thy paths.");
+        Reference _reference = new Reference("Proverbs", 3, 5, 2, 6);
+        Scripture scripture = new Scripture(_reference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
 
         while (userInput != "quit")
         {
