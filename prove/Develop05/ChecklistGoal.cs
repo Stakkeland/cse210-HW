4eea622672342fb50818f0a9aec8d273b5e5eb83
Stakkeland <sawyerstakkeland@gmail.com>
2023-07-08 17:51:57 -0700

    All ready to the best of my ability

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,3 +1,4 @@
+using System.IO; 
 public class ChecklistGoal : Goal
 {
     //variables
@@ -22,6 +23,54 @@ public class ChecklistGoal : Goal
 
 
         File.AppendAllLines(@"C:\Users\Sawyer Stakkeland\OneDrive\Documents\BYUI\2023 Spring\Classes\cse210-HW\prove\Develop05\points.txt", records);
+    }
+
+    public void CompleteChecklist()
+    {
+        string filename = "points.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            string checklistGoalCounter = parts[5];
+            string checklistGoalTotal = parts[6];
+            string bonusPoints = parts[7];
+
+            if (checklistGoalCounter == "placeholder")
+            {
+                return;
+            }
+            else if (checklistGoalTotal == "placeholder")
+            {
+                return;
+            }
+            else
+            {
+                int cgc = Convert.ToInt32(checklistGoalCounter);
+                int cgt = Convert.ToInt32(checklistGoalTotal);
+                int bp = Convert.ToInt32(bonusPoints);
+                if (cgc < cgt)
+                {
+                    return;
+                }
+                else if (cgc == cgt)
+                {
+                    string str1 = File.ReadAllText(@"C:\Users\Sawyer Stakkeland\OneDrive\Documents\BYUI\2023 Spring\Classes\cse210-HW\prove\Develop05\points.txt");
+                    str1 = str1.Replace("[c]","[X]");
+                    File.WriteAllText(@"C:\Users\Sawyer Stakkeland\OneDrive\Documents\BYUI\2023 Spring\Classes\cse210-HW\prove\Develop05\points.txt", str1);
+
+                    string[] lines1 = System.IO.File.ReadAllLines(@"C:\Users\Sawyer Stakkeland\OneDrive\Documents\BYUI\2023 Spring\Classes\cse210-HW\prove\Develop05\pointcounter.txt");
+                    int convertedpointcounter = Convert.ToInt32(lines1[0]);
+                    int finalnum = bp + convertedpointcounter;
+                    string myString = finalnum.ToString();
+                    lines1[0] = myString;
+                    System.IO.File.WriteAllLines(@"C:\Users\Sawyer Stakkeland\OneDrive\Documents\BYUI\2023 Spring\Classes\cse210-HW\prove\Develop05\pointcounter.txt", lines1);
+                }
+            
+            }
+        }
     }
     public override bool IsComplete()
     {
